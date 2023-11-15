ee726654d6ab8fa9cb686e12395dba1230418254
cristinamedrom <cristinamedranoroman@gmail.com>
2023-11-15 19:01:00 +0100

    ejercicios

--- index.js
+++ index.js
@@ -3,36 +3,44 @@ const _ = require('lodash');
 
 function sum(a, b) {
   // Use de add() function from lodash to sum the two numbers
-  console.log(_.sum([a, b]));
+  return _.sum([a, b]);
 }
 
 function filterFalsyValues(arr) {
   // Use the compact() function from lodash to filter the array
-
+  return _.compact(arr);
 }
 
 function differenceInArrays(firstArray, secondArray) {
   // Use the difference() function from lodash to get the difference between the two arrays
+  return _.difference(firstArray, secondArray);
 }
 
 function fillArray(arr, value) {
   // Use the fill() function from lodash to fill the array with the value
+  const filledArray = _.fill(arr, value);
+  return filledArray;
 }
 
 function calculateMean(arr) {
   // Use the mean() function from lodash to calculate the mean of the array
+  const calculatedMean = _.mean(arr);
+  return calculatedMean;
 }
 
 function camelCase(str) {
   // Use the camelCase() function from lodash to convert the string to camelCase
+  return _.camelCase(str);
 }
 
 function capitalize(str) {
   // Use the capitalize() function from lodash to capitalize the string
+  return _.capitalize(str);
 }
 
 function sortByNames(arr) {
   // Use the sortBy() function from lodash to sort the array by name
+  return _.sortBy(arr);
 }
 
 function flatArrayAndCalculateMean(arr) {
