a5048dfdeeb59480d1e2e580ee2faa6efdc77461
cristinamedrom <cristinamedranoroman@gmail.com>
2023-11-15 21:01:20 +0100

    faltan los dos ultimos

--- index.js
+++ index.js
@@ -40,22 +40,28 @@ function capitalize(str) {
 
 function sortByNames(arr) {
   // Use the sortBy() function from lodash to sort the array by name
-  return _.sortBy(arr);
+  return _.sortBy(arr, ['name']);
 }
 
 function flatArrayAndCalculateMean(arr) {
   // Use the mean() function from lodash to calculate the mean of the array
   // Use the flatten() function from lodash to flatten the array
+  const flated = _.flatten(arr);
+  return _.mean(flated);
 }
 
 function sortByNamesAndCapitalizeFirstLetter(arr) {
   // Use the sortBy() function from lodash to sort the array by name
   // Use the capitalize() function from lodash to capitalize the first letter of each name
+  const sortedArray = _.sortBy(arr, ['name']);
+  return _.capitalize(sortedArray);
 }
 
 function transformToSnakeCaseAndAllCaps(arr) {
   // Use the snakeCase() function from lodash to convert the string to snake_case
   // Use the toUpper() function from lodash to convert the string to uppercase
+  const snakedArray = _.snakeCase(arr);
+  return _.toUpper(snakedArray);
 }
 
 module.exports = {
